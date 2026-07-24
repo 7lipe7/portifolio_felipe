0cb9f0352d407a30ece71da47bd647bbf2535ef6
felipe <fsantanalopes@gmail.com>
2026-07-24 13:39:36 -0300

    Add new icons and create Habili page
    - Added icons for PHP, MySQL, Node.js, Prisma ORM, and Python.
    - Created a new page component: Habili.jsx.

--- Portifolio_Felipe/src/pages/Info.jsx
+++ Portifolio_Felipe/src/pages/Info.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './Info.css'
 import react from '../assets/icons8-reagir-96.png'
+import php from '../assets/icons8-logo-php-96.png'
+import mySql from '../assets/icons8-meu-sql-96.png'
+import node from '../assets/icons8-nodejs-96.png'
+import c from '../assets/icons8-c++-96.png'
+import py from '../assets/icons8-python-96.png'
+import prisma from '../assets/icons8-prisma-orma-96.png'
+import java from '../assets/icons8-logo-java-coffee-cup-96.png'
+import js from '../assets/icons8-javascript-96.png'
+
 
 const Info = () => {
   return (
@@ -19,45 +28,83 @@ const Info = () => {
             <div className="btn-tec">
               <div className="box-btn">
                 <div className="fig">
-                    <button>React</button>
-                  <img src={react } style={{width:'50px', margin:'0 auto'}} alt="" />
+                  <button>React</button>
+                  <img src={react} style={{ width: '50px', margin: '0 auto' }} alt="" />
                 </div>
-              
-                
+
+
+              </div>
+
+              <div className="box-btn">
+                <div className="fig">
+                  <button>php</button>
+                  <img src={php} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
+
               </div>
               <div className="box-btn">
-                <button>PHP</button>
+                <div className="fig">
+                  <button>sql</button>
+                  <img src={mySql} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
+
+              </div>
+
+              <div className="box-btn">
+                <div className="fig">
+                  <button>javaScript</button>
+                  <img src={js} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
+
+              </div>
+
+              <div className="box-btn">
+                <div className="fig">
+                  <button>nodeJS</button>
+                  <img src={node} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
+              </div>
+
+
+              <div className="box-btn">
+                <div className="fig">
+                  <button>c++</button>
+                  <img src={c} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
+              </div>
+
+
+              <div className="box-btn">
+                <div className="fig">
+                  <button>python</button>
+                  <img src={py} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
               </div>
               <div className="box-btn">
-                <button>MySQL</button>
+                <div className="fig">
+                  <button>prisma.js</button>
+                  <img src={prisma} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
               </div>
               <div className="box-btn">
-                <button>Figma</button>
+                <div className="fig">
+                  <button>java</button>
+                  <img src={java} style={{ width: '50px', margin: '0 auto' }} alt="" />
+                </div>
               </div>
-              <div className="box-btn">
-                <button>Node.js</button>
-              </div>
-              <div className="box-btn">
-                <button>c++</button>
-              </div>
-                 <div className="box-btn">
-                <button>pyhton</button>
-              </div>
-              <div className="box-btn">
-                <button>prisma</button>
-              </div>
-              <div className="box-btn">
-                <button>java</button>
-              </div>
+
 
 
 
             </div>
-            
 
+          
+
+             
+         
 
           </div>
-          
+
 
           <div className="text">
             <h2>Quem sou eu</h2>
