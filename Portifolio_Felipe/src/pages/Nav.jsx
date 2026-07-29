62260dc2cccf80a2bdebff78cd83e79960b70771
felipe <fsantanalopes3@gmail.com>
2026-07-28 21:35:28 -0300

    fix: Atualizar link do currículo para usar variável de importação

--- Portifolio_Felipe/src/pages/Nav.jsx
+++ Portifolio_Felipe/src/pages/Nav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './style/Nav.css'
 
 import imgDowload from '../assets/icons8-baixar-32.png'
+import cvFile from '../assets/currículo_Felipe.pdf'
 
 const scrollToSection = (id) => {
   const element = document.getElementById(id)
@@ -41,7 +42,7 @@ const Nav = () => {
                     <button onClick={() => handleNavClick('projetos')}>projetos</button>
                     <button onClick={() => handleNavClick('contato')}>contato</button>
                 </div>
-                <a href="src/assets/currículo_Felipe.pdf" download className={`cv-link ${isMenuOpen ? 'show' : ''}`}>
+                <a href={cvFile} download className={`cv-link ${isMenuOpen ? 'show' : ''}`}>
                   <button id='Cv-download'><img src={imgDowload} alt="download" style={{width:'18px',height:'18px', marginRight:'5px'}} /> baixar cv</button>
                 </a>
             </div>
